cf26ac6dd46e80846b370033afe5b4e83f77297a
Shreya Sanjay <shreyasanjay007@gmail.com>
2025-01-16 11:51:15 +0530

    designed dropdown animation

--- script.js
+++ script.js
@@ -1,26 +1,79 @@
-const faqs = document.getElementsByClassName("faqs");
-console.log(faqs);
 
-for(let i=0; i<faqs.length;i++){
-    let isFaqOpen = false
-    const ans = document.createElement("div")
-    faqs[i].addEventListener("click",()=>{
-        console.log('from add eevent listner boloc' ,isFaqOpen)
-        if(isFaqOpen){
-            // faqs[i].removeChild(faqs[i].lastChild)
-            console.log(faqs[i].lastChild)
-            console.log('from if block',isFaqOpen)
-        }
-        console.log("clicked")
-       
-        ans.innerHTML = "This is answer"
-        ans.style.height = "50vh"
-        ans.style.width = "80vw"
-        ans.style.backgroundColor = "#2d2d2d"
-        isFaqOpen = true
-        faqs[i].appendChild(ans)
-        
-    })
+
+const faqs = document.getElementsByClassName("faqs");
+const question = document.getElementsByClassName("question");
+
+
+for(let i=0; i<faqs.length; i++){
+    let flag = false;
+        const plus = document.getElementsByClassName("plus")[i];
+        const answer = document.getElementsByClassName("answer")[i];
+
+
+    question[i].addEventListener('click',()=>{
+    
+    plus.classList.toggle('rotate');
+    // answer.classList.toggle('animate')
+    answer.innerHTML = faqsArray[i];
+    if(answer.classList.contains("animate")){
+        answer.classList.remove("animate")
+        answer.classList.add("animate-off")
+    }
+    else{
+        answer.classList.remove("animate-off")
+        answer.classList.add("animate")
+    }
+
+    flag = true;
+})
+
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const faqsArray = [
+    `Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries and more - on thousands of internet-connected devices.
+     <br> </br>
+You can watch as much as you want, whenever you want, without a single ad – all for one low monthly price. There's always something new to discover, and new TV shows and movies are added every week!`,
+
+ `Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from ₹149 to ₹649 a month. No extra costs, no contracts.`,
+
+`Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles.
+
+You can also download your favourite shows with the iOS or Android app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere.`,
+
+`Netflix is flexible. There are no annoying contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.`,
+
+`Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want.`,
+
+`The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and films in their own space.
+
+Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see.`
+];
